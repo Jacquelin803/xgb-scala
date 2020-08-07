cd33afdb649a89458090a5b70b6c5c282d3b141c
Jacquelin1 <44801887+Jacquelin1@users.noreply.github.com>
2020-08-07 17:53:28 +0800

    Add files via upload

---
+++ predict/src/main/java/com/eg/xgb/jpmml/load/JPMMLModelServing.java
@@ -0,0 +1,43 @@
+package com.eg.xgb.jpmml.load;
+
+
+import org.dmg.pmml.FieldName;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JPMMLModelServing {
+    public static void main(String[] args){
+
+        JavaModelServer javaModelServer = new JavaModelServer("/opt/temp/xgb_cv.pmml");
+
+        HashMap<String, Object> featureMap = new HashMap<>();
+        //binary or multi
+        featureMap.put("sepal_length", 5.1);
+        featureMap.put("sepal_width", 3.5);
+        featureMap.put("petal_length", 1.4);
+        featureMap.put("petal_width", 0.2);
+
+        //reg
+//        featureMap.put("RM", 3.863);
+//        featureMap.put("LSTAT", 13.33);
+//        featureMap.put("PTRATIO", 20.2);
+
+;
+
+
+
+        Map<FieldName, ?> result = javaModelServer.forecast(featureMap);
+
+        for (Map.Entry<FieldName, ?> field : result.entrySet()){
+            System.out.println(field.getKey().getValue() + ":\t" +  field.getValue());
+        }
+
+        for(int i = 0 ; i < result.size(); i++){
+            System.out.println(result);
+        }
+
+//        System.out.println(result.get(new FieldName("probability(1)")));
+
+    }
+}
